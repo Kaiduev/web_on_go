87434e0942ccb8e0ae361066904d23b464a01eb1
Baktygul Kaiduev <you@example.com>
2020-08-28 14:44:44 +0600

    first commit

---
+++ router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"dentist/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/users", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
+	return router
+}
